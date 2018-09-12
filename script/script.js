0e7c2bcce9e1078f8660f1c72b909ecd0da1a5ca
Usagino <yuki.itoh@front-artgraph.com>
2018-09-12 11:58:00 +0900

    update

--- script/script.js
+++ script/script.js
@@ -1,58 +1,63 @@
-
-
 $(function(){
+
+  //画面高さ取得
+
+  let half = $(window).height() / 2;
+
+  let flex_length = $(".flex-pos div").length
+
   $.scrollify({section:".contents"});
+
+  $(`.flex-ham`).click(function(event) {
+    $(`.hamburger-menu`).css('right', '0%');
+    $(`.hamburger-menu_nav`).css('right', '0%');
+  });
+  $(`.hamburger-menu`).click(function(event) {
+    $(`.hamburger-menu`).css('right', '');
+    $(`.hamburger-menu_nav`).css('right', '');
+  });
 
   let pos_list = [],content_pos,pos;
 
-
-  for (let i = 1; i <= 4; i++) {
+  for (let i = 1; i <= flex_length; i++) {
     content_pos = $(`.content-${i}`).offset().top;
-    pos_list.push(content_pos);
+    pos_list.push(content_pos - half);
     console.log(`content-${i} の高さは${content_pos}`);
   }
 
-  for (let i = 1; i <= 4; i++) {
+  for (let i = 1; i <= flex_length; i++) {
     if(pos_list[i] >= pos){
       console.log(`Hello-${i}`);
     }
   }
-  const posanime = () =>{
+  const pos_anime = () =>{
     pos =  $(window).scrollTop() + 1;
-    console.log(pos);
-    if(pos_list[0] <= pos && pos_list[1] >= pos){
-      console.log(`今は1`);
-      $('.flex-pos div:nth-child(1)').addClass('double');
-    }else {
-      $('.flex-pos div:nth-child(1)').removeClass('double');
-    }
-
-    if(pos_list[1] <= pos && pos_list[2] >= pos){
-      console.log(`今は2`);
-      $('.flex-pos div:nth-child(2)').addClass('double');
-    }
-    else{
-      $('.flex-pos div:nth-child(2)').removeClass('double');
-    }
-
-    if(pos_list[2] <= pos && pos_list[3] >= pos){
-      console.log(`今は3`);
-      $('.flex-pos div:nth-child(3)').addClass('double');
-    }else{
-      $('.flex-pos div:nth-child(3)').removeClass('double');
-    }
-
-    if(pos_list[3] <= pos){
-      console.log(`今は4`);
-      $('.flex-pos div:nth-child(4)').addClass('double');
-    }else{
-      $('.flex-pos div:nth-child(4)').removeClass('double');
+    for (let i = 0; i <= flex_length; i++) {
+      if(pos_list[i] <= pos && !(pos_list[ i+1 ] <= pos)){
+        console.log(`今は${i+1}階`);
+        $(`.flex-pos div:nth-child(${i+1})`).addClass('double');
+      }else {
+        $(`.flex-pos div:nth-child(${i+1})`).removeClass('double');
+      }
     }
   }
-  posanime();
+
+  pos_anime();
 
 
   $(window).scroll(function(event) {
-    posanime();
+    pos_anime();
   });
+
+
+  let link_array = ['twitter','instagram','wantedly'];
+  for(let icon of link_array){
+    $(`.link_${icon}`).hover(function() {
+      $(`.contact_icon_${icon}`).css('right', '0%');
+
+    }, function() {
+      $(`.contact_icon_${icon}`).css('right', '100%');
+    });
+  }
+
 });
